67f347c24d24770d79050327e60117a93da9cff6
anna <ani.kolesnikova@gmail.com>
2023-06-13 14:04:05 +0200

    feat: implement src/cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const userInputArgs = process.argv.slice(2);
+  const cliArgs = userInputArgs.reduce((acc, arg, i, arr) => {
+    const probableValue = arr[i + 1];
+    if (probableValue && arg.startsWith("--")) {
+      const modifiedArgs = arg.slice(2);
+      const modifiedCliArgs = `${modifiedArgs} is ${probableValue}`;
+      acc.push(modifiedCliArgs);
+    }
+    return acc;
+  }, []);
+
+  console.log(cliArgs.join(", "));
 };
 
 parseArgs();
