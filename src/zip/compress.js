db8bceb8e1780f22a010310f9723345c26a68d58
anna <ani.kolesnikova@gmail.com>
2023-06-16 14:45:41 +0200

    feat: add src/zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { getPathFromFiles } from "../getPath.js";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const fileToCompressSrc = getPathFromFiles(
+    import.meta.url,
+    "fileToCompress.txt"
+  );
+  const compressedFileSrc = getPathFromFiles(import.meta.url, "archive.gz");
+  const inputFile = createReadStream(fileToCompressSrc);
+  const outputFile = createWriteStream(compressedFileSrc);
+  inputFile.pipe(outputFile);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import zlib from "zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { getPathFromFiles } from "../getPath.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const gzip = zlib.createUnzip();
+  const compressedFileSrc = getPathFromFiles(import.meta.url, "archive.gz");
+  const uncompressedFileSrc = getPathFromFiles(
+    import.meta.url,
+    "fileToCompress.txt"
+  );
+
+  const inputFile = createReadStream(compressedFileSrc);
+  const outputFile = createWriteStream(uncompressedFileSrc);
+  inputFile.pipe(outputFile);
 };
 
 await decompress();
