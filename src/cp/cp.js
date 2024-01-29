2915c0c74260fa90efeac7d93d554d3dd41f6eab
anna <ani.kolesnikova@gmail.com>
2024-01-29 20:47:26 +0100

    feat: nodejs basics - advanced scope

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { getPath } from "../getPath.js";
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = getPath(import.meta.url, "files", "script.js");
+    const child = spawn("node", [scriptPath, ...args.split(" ")]);
+  
+    process.stdin.on("data", (data) => {
+      child.stdin.write(data);
+    });
+  
+    child.stdout.on("data", (data) => {
+      console.log(`stdout:\n${data}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("--silent --all");
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { getPath } from "../getPath.js";
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cp = cpus();
+    let count = 10;
+
+    const workerPath = getPath(import.meta.url, "", "./worker,js");
+    const worker = await Promise.allSettled(
+        cp.map(() => {
+            return new Promise((resolve, reject) => {
+                const newWorker = new Worker(workerPath, {
+                    workerData: count++,
+                })
+                newWorker.on("message", (message) => resolve(message));
+                newWorker.on("error", (message) => reject(message));
+            })
+        })
+    );
+
+    const results = worker.map(({status, value}) => ({
+        status: status === "fulfilled" ? "resolved" : "error",
+        data: status === "fulfilled" ? value : null,
+    }));
+
+    console.log(results);
+    return results;
 };
 
 await performCalculations();
