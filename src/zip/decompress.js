9ce269ce96ed6d157949327db7865355e2f41fc5
anna <ani.kolesnikova@gmail.com>
2024-10-06 15:51:10 +0200

    feat: zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import zlib from "zlib";
+import { getPath } from "../getPath.js";
+import { createReadStream, createWriteStream } from "node:fs";
+import { rm } from "node:fs/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompress = getPath(import.meta.url, "files", "archive.gz");
+  const decompressedFile = getPath(
+    import.meta.url,
+    "files",
+    "fileToCompress.txt"
+  );
+  try {
+    const source = createReadStream(fileToDecompress);
+    const destination = createWriteStream(decompressedFile);
+    const unzip = zlib.createUnzip();
+    source.pipe(unzip).pipe(destination);
+    rm(fileToDecompress);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await decompress();
